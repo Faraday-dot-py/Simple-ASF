bb609fb689f712c5c5b2aa58e5bdb9a5edf7b9e0
Adam Webb <adameternal44@gmail.com>
2024-01-07 00:40:58 -0800

    Figured out firebase and deleted unnecessary code

--- src/Container.js
+++ src/Container.js
@@ -1,37 +1,32 @@
-
 /**
- *  --- TODO ---
- * Firebase backend -- COMPLETE
- * Scouting Visualization With Simple ASF???
- * Required Fields -- COMPLETE
- * Optional upper and lower bounds for counters -- COMPLETE
- * Save data to local storage -- COMPLETE
- * Write an actual function to clear the form -- COMPLETE
- * Rewrite the whole damn thing :heart: -- COMPLETE
-*/
+ * TODO:
+ * 
+ * Format:
+ * - Top-level reference to firebase
+ * - When firebase is updated, update the state, which rerenders the page
+ *   - No use in rerendering only one widget bc an entire match is submitted at once, no reason to update only one widget
+ * - 
+ */
 
 import './App.css';
 
-import CheckBox from './widgets/CheckBox';
-import TextBox from './widgets/TextBox'
-import TextBoxLong from './widgets/TextBoxLong'
-import Counter from './widgets/Counter'
-import Dropdown from './widgets/Dropdown';
-
 import layout from './layout.json'
-import {firebaseConfig, sortMetrics, renderRequiredStars} from './Config.js'
+import {firebaseConfig, sortMetrics } from './Config.js'
 
 import logo from './images/patribotsLogo.png';
 
 import {v4 as uuidv4} from "uuid"
 import React from 'react';
 
-import { getDatabase, ref, update } from "firebase/database";
+import { getDatabase, ref, onValue, set } from "firebase/database";
 import { initializeApp } from "firebase/app";
-
+import BarGraph from './widgets/BarGraph.js';
 
 const app = initializeApp(firebaseConfig);
 
+const db = getDatabase();
+
+const dbRef = ref(db, '/');
 
 
 // Just a lil QOL function to add ids to the layout
@@ -40,39 +35,74 @@ Object.keys(layout).forEach((item) => {
   layout[item].id = uuidv4()
 })
 
-class Container extends React.Component{
-  
+/*
+
+export const data = [
+  ["Year", "Sales", "Expenses", "Profit"],
+  ["2014", 1000, 400, 200],
+  ["2015", 1170, 460, 250],
+  ["2016", 660, 1120, 300],
+  ["2017", 1030, 540, 350],
+];
+
+export const options = {
+  chart: {
+    title: "Company Performance",
+    subtitle: "Sales, Expenses, and Profit: 2014-2017",
+  },
+};
+
+export function App() {
+  return (
+    <Chart
+      chartType="Bar"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+    />
+  );
+}
+ */
+
+class Container extends React.Component {
   state = {
-    interactables: layout.filter((item) => (isInteractable(item.type)))
+    interactables: layout.filter((item) => item),
+    matchData: undefined
+  };
+
+  onDatabaseUpdate = (snapshot) => {
+    const data = snapshot.val();
+    console.log("Db was updated:", data);
+    this.setState({ matchData: data });
+  };
+
+  componentDidMount() {
+    onValue(dbRef, this.onDatabaseUpdate);
   }
 
-  
-
-  render () {
+  render() {
     return (
       <div className="app">
-        <image src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR71FESRUZEUb2_52JI8EZhNkaQ3fn0p1vFU8UANUePHUI2qQbzsFqS12lnWCv1Fj6S87k&usqp=CAU"}></image>
-        <ul className="outer-container">
-          {Object.keys(layout).map((item) => {   
-            if (isInteractable(layout[item].type)) {
-            }
+        {Object.keys(layout).map((item) => {
             switch (layout[item].type) {
               case "header":
-                return <h1          id={layout[item].id} className={"widget header " + layout[item].decorator}>{layout[item].value}</h1>
+                return <h1           id={layout[item].id} className={"widget header " + layout[item].decorator}>{layout[item].value}</h1>
               case "label":
-                return <h3          id={layout[item].id} className={"widget label " + layout[item].decorator}>{layout[item].value}</h3>
-              case "counter":
-                return <Counter     id={layout[item].id} title={layout[item].title + (renderRequiredStars ? (layout[item].required ? " *" : "") : "")} decorator={layout[item].decorator} value={layout[item].value} resetToValue={layout[item].resetToValue} changeHandler={this.handleCounterChange} increment={layout[item].increment} maxValue={layout[item].maxValue} minValue={layout[item].minValue}/>
+                return <h3           id={layout[item].id} className={"widget label " + layout[item].decorator}>{layout[item].value}</h3>
+              case "barGraph":
+                return <BarGraph     id={layout[item].id} title={layout[item].title} subtitle={layout[item].subtitle} decorator={layout[item].decorator} xLabel={layout[item].xLabel} yLabel={layout[item].yLabel} sortMetrics={layout[item].sortMetrics} data={layout[item].data}/>
               default:
                 return <div>error: undefined widget type [{layout[item.type]}]</div>
             }
           })}
-          
-          <img src={logo} alt="" style={{height:0,width:0}} onClick={this.c}/>
-        </ul>
       </div>
     );
   }
 }
 
 export default Container;
+
+/* {
+          
+          <img src={logo} alt="" style={{height:0,width:0}} onClick={this.c}/> */
